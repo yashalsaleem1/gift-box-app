9a57a433012ec2577efd3f9f6d59ae67d4ccc106
yashalsaleem1 <yashalsaleem116@gmail.com>
2025-12-01 12:50:20 +0500

    Hero banner responsiveness fixed

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -13,10 +13,16 @@ const HeroBanner = ({
   const navigate = useNavigate();
 
   return (
-    <div className={`hero-banner ${className || ""}`}>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <CustomButton className="shop-btn" onClick={() => navigate(buttonLink)}>
+    <div
+      className={`hero-banner container text-center py-5 px-3 mt-5${className || ""}`}
+    >
+      <h1 className="display-4 fw-bold">{title}</h1>
+      <p className="lead mx-auto mb-4">{description}</p>
+
+      <CustomButton
+        className="shop-btn btn btn-warning px-4 py-2 fw-semibold"
+        onClick={() => navigate(buttonLink)}
+      >
         {buttonLabel}
       </CustomButton>
     </div>
